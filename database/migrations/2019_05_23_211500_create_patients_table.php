15450b940e09054ba78c08a461ad4935886ae96f
Janathan <medero.janathan@gmail.com>
2019-05-23 17:47:25 -0500

    Migracion de pacientes creada

--- database/migrations/2019_05_23_211500_create_patients_table.php
+++ database/migrations/2019_05_23_211500_create_patients_table.php
@@ -15,6 +15,21 @@ class CreatePatientsTable extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->Increments('id');
+            $table->string('name');
+            $table->string('picture')->default('default.png');
+            $table->string('address');
+            $table->string('city');
+            $table->date('birthdate');
+            $table->integer('age');
+            $table->integer('phone_1');
+            $table->integer('phone_2');
+            $table->integer('email');
+            $table->boolean('gender');
+            $table->integer('pregnancy')->nullable();
+            $table->integer('lactation')->nullable();
+            $table->double('weight', 8,2);
+            $table->double('size', 3,1);
+            $table->text('notes');
             $table->timestamps();
         });
     }
